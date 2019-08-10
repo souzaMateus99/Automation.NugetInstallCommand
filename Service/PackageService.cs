7dbc853894a40dcc5848bbd1e8024a55c07bd870
souzaMateus99 <mateussam.souza@gmail.com>
2019-08-10 18:35:31 -0300

    Criando camada de servico para adicionar referencia ao projeto

---
+++ Service/PackageService.cs
@@ -0,0 +1,12 @@
+﻿using DTO;
+using System;
+
+namespace Service
+{
+    public class PackageService
+    {
+        public void AddProjectReference(PackageDTO packageDTO, string projectPath){
+            throw new NotImplementedException();
+        }
+    }
+}
